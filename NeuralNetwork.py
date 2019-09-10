756258f936c4a5644c286f7ccee083114f49cbe3
Romain <romain.fouyer@free.fr>
2019-09-10 23:12:35 +0200

    * Modified existent

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-from Utils import sigmoid, sigmoid_derivative
-
 
 class NeuralNetwork:
-    def __init__(self, x, y):
+    def __init__(self, x, y, activation_function, activation_function_derivative):
         self.input = x
         self.weights1 = np.random.rand(self.input.shape[1], 4)
         self.weights2 = np.random.rand(4, 1)
         self.y = y
         self.output = np.zeros(self.y.shape)
+        self.activation_function = activation_function
+        self.activation_function_derivative = activation_function_derivative
 
     def feedforward(self):
         '''
@@ -18,8 +18,8 @@ class NeuralNetwork:
                 -> on calcule alors à partir des poids la couche intermédiaire pour l'output
                 -> on determine ensuite l'output de notre feedforward
         '''
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+        self.layer1 = self.activation_function(np.dot(self.input, self.weights1))
+        self.output = self.activation_function(np.dot(self.layer1, self.weights2))
 
     def backprop(self):
         '''
@@ -31,9 +31,15 @@ class NeuralNetwork:
         '''
 
         d_weights2 = np.dot(
-            self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(
-            self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+                            self.layer1.T,
+                            (2 * (self.y - self.output) * self.activation_function_derivative(self.output))
+                            )
+        d_weights1 = np.dot(
+                            self.input.T,
+                            (np.dot(2*(self.y - self.output) *
+                            self.activation_function_derivative(self.output), self.weights2.T) *
+                             self.activation_function_derivative(self.layer1))
+                            )
 
         self.weights1 += d_weights1
         self.weights2 += d_weights2
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from NeuralNetwork import NeuralNetwork
+from Utils import sigmoid, sigmoid_derivative
 
 if __name__ == "__main__":
     X = np.array([[0, 0],
@@ -8,7 +9,7 @@ if __name__ == "__main__":
                   [0, 1],
                   [1, 1]])
     y = np.array([[0], [1], [1], [0]])
-    nn = NeuralNetwork(X, y)
+    nn = NeuralNetwork(X, y, sigmoid, sigmoid_derivative)
 
     for i in range(20000):
         nn.feedforward()
